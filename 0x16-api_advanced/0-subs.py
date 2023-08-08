c93e5e6c7c3238dc6b811cdb7529765532f4bee2
iamjoee <joeadvocates123@gmail.com>
2023-08-08 21:48:01 +0000

    git commit -m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
+
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "MyRedditBot/1.0"  # Set your custom User-Agent here
     }
@@ -23,5 +24,5 @@ def number_of_subscribers(subreddit):
 if __name__ == '__main__':
     subreddit_name = "programming"
     subscribers = number_of_subscribers(subreddit_name)
-    print(f"The subreddit '{subreddit_name}' has {subscribers} subscribers.")
+    print("The subreddit '{}' has {} subscribers.".format(subreddit_name, subscribers))
 
