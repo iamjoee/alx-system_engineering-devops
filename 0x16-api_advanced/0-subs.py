3329072e074e84bc1494e4630241cfb78854cbc7
iamjoee <joeadvocates123@gmail.com>
2023-08-08 14:33:25 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""this function queries subscribers""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "MyRedditBot/1.0"  # Set your custom User-Agent here
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
+    
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    
+    try:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    except (KeyError, ValueError):
+        return 0
+
+if __name__ == '__main__':
+    subreddit_name = "programming"
+    subscribers = number_of_subscribers(subreddit_name)
+    print(f"The subreddit '{subreddit_name}' has {subscribers} subscribers.")
+
