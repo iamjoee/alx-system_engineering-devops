90409e89d40e00eec21286ce58f3db6d10f45ec0
iamjoee <joeadvocates123@gmail.com>
2023-08-09 21:32:53 +0000

    0x17. Web stack debugging #3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-"""this function prints hot posts on a given Reddit subreddit.
+"""Function to print titles of top 10 hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """Print the titles of the top 10 hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    try:
+        data = response.json().get("data")
+        children = data.get("children")
+
+        for post in children[:10]:
+            print(post.get("data").get("title"))
+
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
